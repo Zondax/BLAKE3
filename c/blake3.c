2b94996bb502104ec45ce6f64c9523768aad1708
abenso <andresbenso@gmail.com>
2025-12-09 13:35:57 -0300

    Fix unused variable warnings for non-x86 platforms
    - blake3.c: Wrap num_cvs variable in preprocessor conditional
    - blake3_dispatch.c: Only define g_cpu_features and get_cpu_features when IS_X86 or BLAKE3_TESTING
    
    These changes fix -Werror,-Wunused-variable and -Werror,-Wunused-function
    errors when compiling for ARM-based Ledger devices with clang static analyzer.

--- c/blake3.c
+++ c/blake3.c
@@ -356,9 +356,16 @@ compress_subtree_to_parent_node(const uint8_t *input, size_t input_len,
 #endif
 
   uint8_t cv_array[MAX_SIMD_DEGREE_OR_2 * BLAKE3_OUT_LEN];
-  size_t num_cvs = blake3_compress_subtree_wide(input, input_len, key,
-                                                chunk_counter, flags, cv_array, use_tbb);
+#if MAX_SIMD_DEGREE_OR_2 > 2
+  size_t num_cvs =
+#else
+  (void)
+#endif
+      blake3_compress_subtree_wide(input, input_len, key,
+                                   chunk_counter, flags, cv_array, use_tbb);
+#if MAX_SIMD_DEGREE_OR_2 > 2
   assert(num_cvs <= MAX_SIMD_DEGREE_OR_2);
+#endif
   // The following loop never executes when MAX_SIMD_DEGREE_OR_2 is 2, because
   // as we just asserted, num_cvs will always be <=2 in that case. But GCC
   // (particularly GCC 8.5) can't tell that it never executes, and if NDEBUG is
